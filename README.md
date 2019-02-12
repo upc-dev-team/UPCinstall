7b57df827bd6df1d37183075d0eb52ea4af291fc
upc-dev-team <47543832+upc-dev-team@users.noreply.github.com>
2019-02-12 14:19:37 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,69 @@
+# UltraPayCoin
+Shell script to install a [UltraPayCoin Masternode](https://ultrapay.info/) on a Linux server running Ubuntu 16.04 . Use it on your own risk.
+
+***
+## Installation (latest wallet version):
+```
+git clone https://github.com/upc-dev-team/UPCinstall.git
+cd UPCinstall
+bash upc-install.sh
+```
+***
+## Update wallet to latest version:
+```
+cd ~/UPCinstall/
+git pull
+bash upc-update.sh
+```
+***
+
+## Desktop wallet setup
+
+After the MN is up and running, you need to configure the desktop wallet accordingly. Here are the steps for Windows Wallet
+1. Open the UltraPayCoin Coin Desktop Wallet.
+2. Go to RECEIVE and create a New Address: **MN1**
+3. Send **6000** **UPC** to **MN1**.
+4. Wait for 15 confirmations.
+5. Go to **Tools -> "Debug console - Console"**
+6. Type the following command: **masternode outputs**
+7. Go to  ** Tools -> "Open Masternode Configuration File"
+8. Add the following entry:
+```
+Alias Address Privkey TxHash Output_index
+```
+* Alias: **MN1**
+* Address: **VPS_IP:PORT**
+* Privkey: **Masternode Private Key**
+* TxHash: **First value from Step 6**
+* Output index:  **Second value from Step 6**
+9. Save and close the file.
+10. Go to **Masternode Tab**. If you tab is not shown, please enable it from: **Settings - Options - Wallet - Show Masternodes Tab**
+11. Click **Update status** to see your node. If it is not shown, close the wallet and start it again. Make sure the wallet is unlocked.
+12. Open **Debug Console** and type:
+```
+startmasternode "alias" "0" "MN1"
+```
+***
+
+## Usage:
+```
+ultrapaycoin-cli getinfo
+ultrapaycoin-cli mnsync status
+ultrapaycoin-cli masternode status
+```
+Also, if you want to check/start/stop **UltraPayCoin** , run one of the following commands as **root**:
+
+**Ubuntu 16.04**:
+```
+systemctl status UltraPayCoin #To check the service is running.
+systemctl start UltraPayCoin #To start UltraPayCoin service.
+systemctl stop UltraPayCoin #To stop UltraPayCoin service.
+systemctl is-enabled UltraPayCoin #To check whetether UltraPayCoin service is enabled on boot or not.
+```
+**Ubuntu 14.04**:  
+```
+/etc/init.d/UltraPayCoin start #To start UltraPayCoin service
+/etc/init.d/UltraPayCoin stop #To stop UltraPayCoin service
+/etc/init.d/UltraPayCoin restart #To restart UltraPayCoin service
+```
+***
---
+++ upc-update.sh
@@ -0,0 +1,150 @@
+#!/bin/bash
+
+CONFIG_FILE='ultrapaycoin.conf'
+CONFIGFOLDER='/root/.ultrapaycoin'
+COIN_DAEMON='/usr/local/bin/ultrapaycoind'
+COIN_CLI='/usr/local/bin/ultrapaycoin-cli'
+COIN_REPO='https://github.com/upc-dev-team/UltraPayCoin/releases/download/1.0/ubuntu16.04-daemon.tar.gz'
+COIN_NAME='UltraPayCoin'
+COIN_PORT=13333
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m'
+
+progressfilt () {
+  local flag=false c count cr=$'\r' nl=$'\n'
+  while IFS='' read -d '' -rn 1 c
+  do
+    if $flag
+    then
+      printf '%c' "$c"
+    else
+      if [[ $c != $cr && $c != $nl ]]
+      then
+        count=0
+      else
+        ((count++))
+        if ((count > 1))
+        then
+          flag=true
+        fi
+      fi
+    fi
+  done
+}
+
+function compile_node() {
+
+  echo -e "Stop the $COIN_NAME wallet daemon"
+  if (( $UBUNTU_VERSION == 16 || $UBUNTU_VERSION == 18 )); then
+    systemctl stop $COIN_NAME.service
+  else
+    /etc/init.d/$COIN_NAME stop
+  fi
+  sleep 7
+  
+  echo -e "Remove the old $COIN_NAME wallet from the system"
+  rm -f /usr/local/bin/ultrapaycoin* >/dev/null 2>&1
+  rm $CONFIGFOLDER/banlist.dat >/dev/null 2>&1
+  rm $CONFIGFOLDER/mnpayments.dat >/dev/null 2>&1
+  rm $CONFIGFOLDER/fee_estimates.dat >/dev/null 2>&1
+  rm $CONFIGFOLDER/peers.dat >/dev/null 2>&1
+  rm $CONFIGFOLDER/budget.dat >/dev/null 2>&1
+  rm $CONFIGFOLDER/mncache.dat >/dev/null 2>&1
+  rm $CONFIGFOLDER/debug.log >/dev/null 2>&1
+  rm $CONFIGFOLDER/db.log >/dev/null 2>&1
+  rm $CONFIGFOLDER/bootstrap.dat >/dev/null 2>&1
+  rm $CONFIGFOLDER/bootstrap.dat.old >/dev/null 2>&1
+  sleep 5
+  clear
+  
+  echo -e "Prepare to download a new wallet of $COIN_NAME"
+  TMP_FOLDER=$(mktemp -d)
+  cd $TMP_FOLDER
+  wget --progress=bar:force $COIN_REPO 2>&1 | progressfilt
+  compile_error
+  COIN_ZIP=$(echo $COIN_REPO | awk -F'/' '{print $NF}')
+  COIN_VER=$(echo $COIN_ZIP | awk -F'/' '{print $NF}' | sed -n 's/.*\([0-9]\.[0-9]\.[0-9]\).*/\1/p')
+  COIN_DIR=$(echo ${COIN_NAME,,}-$COIN_VER)
+  tar xvzf $COIN_ZIP --strip=2 ${COIN_DIR}/bin/${COIN_NAME,,}d ${COIN_DIR}/bin/${COIN_NAME,,}-cli>/dev/null 2>&1
+  compile_error
+  rm -f $COIN_ZIP >/dev/null 2>&1
+  cp ultrapaycoin* /usr/local/bin >/dev/null 2>&1
+  compile_error
+  strip $COIN_DAEMON $COIN_CLI
+  cd - >/dev/null 2>&1
+  rm -rf $TMP_FOLDER >/dev/null 2>&1
+  clear
+  
+  echo -e "Start the $COIN_NAME wallet daemon"
+  if (( $UBUNTU_VERSION == 16 || $UBUNTU_VERSION == 18 )); then
+    systemctl start $COIN_NAME.service
+  else
+    /etc/init.d/$COIN_NAME start
+  fi
+  sleep 7
+  clear
+}
+
+function compile_error() {
+if [ "$?" -gt "0" ];
+ then
+  echo -e "${RED}Failed to compile $COIN_NAME. Please investigate.${NC}"
+  exit 1
+fi
+}
+
+function detect_ubuntu() {
+ if [[ $(lsb_release -d) == *18.04* ]]; then
+   UBUNTU_VERSION=18
+ elif [[ $(lsb_release -d) == *16.04* ]]; then
+   UBUNTU_VERSION=16
+ elif [[ $(lsb_release -d) == *14.04* ]]; then
+   UBUNTU_VERSION=14
+else
+   echo -e "${RED}You are not running Ubuntu 14.04, 16.04 or 18.04 Installation is cancelled.${NC}"
+   exit 1
+fi
+}
+
+function checks() {
+ detect_ubuntu 
+if [[ $EUID -ne 0 ]]; then
+   echo -e "${RED}$0 must be run as root.${NC}"
+   exit 1
+fi
+}
+
+function prepare_system() {
+echo -e "Prepare the system to update ${GREEN}$COIN_NAME${NC} master node."
+apt-get update >/dev/null 2>&1
+apt-get install -y wget curl ufw binutils net-tools >/dev/null 2>&1
+}
+
+function important_information() {
+ echo
+ echo -e "================================================================================"
+ echo -e "$COIN_NAME Masternode wallet is update and running, listening on port ${RED}$COIN_PORT${NC}."
+ echo -e "Configuration file is: ${RED}$CONFIGFOLDER/$CONFIG_FILE${NC}"
+ if (( $UBUNTU_VERSION == 16 || $UBUNTU_VERSION == 18 )); then
+   echo -e "Start: ${RED}systemctl start $COIN_NAME.service${NC}"
+   echo -e "Stop: ${RED}systemctl stop $COIN_NAME.service${NC}"
+   echo -e "Status: ${RED}systemctl status $COIN_NAME.service${NC}"
+ else
+   echo -e "Start: ${RED}/etc/init.d/$COIN_NAME start${NC}"
+   echo -e "Stop: ${RED}/etc/init.d/$COIN_NAME stop${NC}"
+   echo -e "Status: ${RED}/etc/init.d/$COIN_NAME status${NC}"
+ fi
+ echo -e "Check if $COIN_NAME is running by using the following command:\n${RED}ps -ef | grep $COIN_DAEMON | grep -v grep${NC}"
+ echo -e "Now update your local wallet (Windows/Mac) and run Masternode from local wallet."
+ echo -e "================================================================================"
+}
+
+##### Main #####
+clear
+
+checks
+prepare_system
+compile_node
+important_information
